77dd57f879be920d9f565437817c3f441d4fcfdd
Tri Ugly <trongtri2410@gmail.com>
2024-06-25 15:40:33 +0700

    Fixed conflict from master

--- extensions/post/api/createPost/createPost[finish].js
+++
@@ -1,9 +0,0 @@
-const createPost = require('../../services/post/createPost');
-
-// eslint-disable-next-line no-unused-vars
-module.exports = async (request, response, delegate) => {
-  const data = request.body;
-  const result = await createPost(data);
-
-  return result;
-};
--- extensions/post/api/deletePost/deletePost[finish].js
+++
@@ -1,9 +0,0 @@
-const deletePost = require('../../services/post/deletePost');
-
-// eslint-disable-next-line no-unused-vars
-module.exports = async (request, response, delegate) => {
-  const { id } = request.params;
-  const result = await deletePost(id);
-
-  return result;
-};
