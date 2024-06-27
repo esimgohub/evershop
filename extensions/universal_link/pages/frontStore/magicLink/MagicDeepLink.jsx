b69ac816c819455147a7876e8c2f87b2ac25534f
Tri Ugly <trongtri2410@gmail.com>
2024-06-27 23:56:51 +0700

    Add universal link

---
+++ extensions/universal_link/pages/frontStore/magicLink/MagicDeepLink.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function MagicLink() {
+  const isMobileScreen = window.innerWidth <= 800 && window.innerHeight <= 600;
+  if (isMobileScreen) {
+    
+  }
+
+  return (
+    <div className="fixed top-0 left-0 bottom-0 right-0 flex justify-center">
+      <Spinner width={30} height={30} className="absolute-center" />
+    </div>
+  );
+}
+
+MagicLink.propTypes = {
+  action: PropTypes.string.isRequired
+};
+
+export const layout = {
+  areaId: 'content',
+  sortOrder: 10
+};
+
