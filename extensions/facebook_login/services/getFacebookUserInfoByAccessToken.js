0423067d1655bd4b03eab2ca5bc8c843dd6f1d41
Nguyen On <nguyenok.dev@gmail.com>
2024-06-28 16:37:17 +0700

    Fix package.json dependency

--- extensions/facebook_login/services/getFacebookUserInfoByAccessToken.js
+++ extensions/facebook_login/services/getFacebookUserInfoByAccessToken.js
@@ -5,7 +5,7 @@ module.exports.getFacebookUserInfoByAccessToken = async (accessToken) => {
   const facebookUserInfoUrl = 'https://graph.facebook.com/v20.0/me';
   const facebookUserInfoQueryOptions = {
     access_token: encodeURIComponent(accessToken),
-    fields: 'id,name,email'
+    fields: 'id,first_name,last_name,email,picture'
   };
 
   try {
@@ -15,7 +15,13 @@ module.exports.getFacebookUserInfoByAccessToken = async (accessToken) => {
     info('Facebook user info response', response.data);
 
     const { data } = response;
-    return data;
+    return {
+      id: data.id,
+      given_name: data.first_name,
+      family_name: data.last_name,
+      email: data.email,
+      picture: data.picture.data.url
+    };
   } catch (err) {
     error(`Failed to get facebook user info ${err.message}`);
     return null;
