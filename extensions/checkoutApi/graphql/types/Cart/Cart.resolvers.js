8cdcdc7f19acd4bae24be16426d46319fbbcd1ce
Nguyen On <nguyenok.dev@gmail.com>
2024-07-04 16:09:59 +0700

    test

--- extensions/checkoutApi/graphql/types/Cart/Cart.resolvers.js
+++ extensions/checkoutApi/graphql/types/Cart/Cart.resolvers.js
@@ -1,10 +1,13 @@
 const { buildUrl } = require('@evershop/evershop/src/lib/router/buildUrl');
 const { camelCase } = require('@evershop/evershop/src/lib/util/camelCase');
-const { getCartByUUID } = require('@evershop/evershop/src/modules/checkout/services/getCartByUUID');
+const {
+  getCartByUUID
+} = require('@evershop/evershop/src/modules/checkout/services/getCartByUUID');
 
 module.exports = {
   Query: {
     cartWithOutArg: async (_, __, { cartId }) => {
+      console.log('abcd');
       try {
         const cart = await getCartByUUID(cartId);
         return camelCase(cart.exportData());
@@ -14,6 +17,7 @@ module.exports = {
     }
   },
   Cart: {
-    updateCartItemApi: (cart) => buildUrl('mobileUpdateCartItems', { cart_id: cart.uuid })
+    updateCartItemApi: (cart) =>
+      buildUrl('mobileUpdateCartItems', { cart_id: cart.uuid })
   }
 };
