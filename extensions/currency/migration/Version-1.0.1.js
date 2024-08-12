e419d2d24a330de1b4589162c2021aa54ab79326
Nguyễn Ngói <nguyenngoi9190@gmail.com>
2024-08-12 15:30:56 +0700

    fix: migration

--- extensions/currency/migration/Version-1.0.1.js
+++ extensions/currency/migration/Version-1.0.1.js
@@ -4,6 +4,7 @@ module.exports = exports = async (connection) => {
   await execute(
     connection,
     `ALTER TABLE customer
+    ADD COLUMN IF NOT EXISTS currency_id uuid,
     ADD CONSTRAINT FK_CUSTOMER_CURRENCY 
     FOREIGN KEY (currency_id) REFERENCES currency (id) ON DELETE SET NULL;`
   );
