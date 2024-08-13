20a045546550198faec885878b902533d88aee4f
Tri Ugly <trongtri2410@gmail.com>
2024-08-14 01:10:02 +0700

    Add delete customer API

---
+++ packages/evershop/src/modules/customer/api/deleteCustomerAccount/deleteCustomerAccount.js
@@ -0,0 +1,26 @@
+const {
+  OK,
+  INTERNAL_SERVER_ERROR
+} = require('@evershop/evershop/src/lib/util/httpStatus');
+const deleteCustomerAccount = require('../../services/customer/deleteCustomerAccount');
+
+// eslint-disable-next-line no-unused-vars
+module.exports = async (request, response, delegate, next) => {
+  try {
+    const customer = await deleteCustomerAccount(request.params.id, {
+      routeId: request.currentRoute.id
+    });
+    response.status(OK);
+    response.json({
+      data: customer
+    });
+  } catch (e) {
+    response.status(INTERNAL_SERVER_ERROR);
+    response.json({
+      error: {
+        status: INTERNAL_SERVER_ERROR,
+        message: e.message
+      }
+    });
+  }
+};
