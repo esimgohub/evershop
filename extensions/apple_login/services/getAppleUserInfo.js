70f507a440db956b061e79e6cae8e1f14aa94c8f
Phung Ng Bao Long <phungngbaolong@gmail.com>
2024-08-16 00:05:40 +0700

    [feat] apple extension

---
+++ extensions/apple_login/services/getAppleUserInfo.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const jwksClient = require('jwks-rsa');
+const jwt = require('jsonwebtoken');
+const { info, error } = require('@evershop/evershop/src/lib/log/logger');
+
+async function getSigningKey(kid) {
+  const client = jwksClient({
+    jwksUri: 'https://appleid.apple.com/auth/keys'
+  });
+
+  const key = await client.getSigningKey(kid);
+  const signingKey = key.getPublicKey();
+  return signingKey;
+
+}
+
+module.exports.getAppleUserInfo = async (id_token) => {
+  try {
+    const decoded = jwt.decode(id_token, { complete: true });
+    const signingKey = await getSigningKey(decoded.header.kid);
+    return jwt.verify(id_token, signingKey, { algorithms: ['RS256'] });
+  } catch (e) {
+    error(e);
+    return null;
+  }
+};
