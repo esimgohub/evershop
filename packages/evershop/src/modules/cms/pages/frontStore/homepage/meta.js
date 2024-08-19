7b5405ba164784b827bc6368fdba0f45a5d6f014
Tri Ugly <trongtri2410@gmail.com>
2024-08-19 11:50:52 +0700

    Change redirect url to login when user access to homepage

--- packages/evershop/src/modules/cms/pages/frontStore/homepage/meta.js
+++ packages/evershop/src/modules/cms/pages/frontStore/homepage/meta.js
@@ -5,14 +5,18 @@ const {
 const { getSetting } = require('../../../../setting/services/setting');
 
 module.exports = async (request, response, delegate, next) => {
-  setContextValue(request, 'pageInfo', {
-    title: await getSetting('storeName', 'EverShop'),
-    description: await getSetting(
-      'storeDescription',
-      'An e-commerce platform with Node and Postgres'
-    ),
-    url: buildUrl('homepage')
-  });
-
-  next();
+  if (!request.isCustomerLoggedIn()) {
+    response.redirect(buildUrl('login'));
+  }
+  else {
+    response.redirect(buildUrl('account'))
+  }
+  // setContextValue(request, 'pageInfo', {
+  //   title: await getSetting('storeName', 'EverShop'),
+  //   description: await getSetting(
+  //     'storeDescription',
+  //     'An e-commerce platform with Node and Postgres'
+  //   ),
+  //   url: buildUrl('homepage')
+  // });
 };
