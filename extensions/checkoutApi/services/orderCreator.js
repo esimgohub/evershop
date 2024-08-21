98af8798aa1ca57489db502e91d6e8dba851c505
Phung Ng Bao Long <phungngbaolong@gmail.com>
2024-08-21 19:45:59 +0700

    [fix] save total_qty order

--- extensions/checkoutApi/services/orderCreator.js
+++ extensions/checkoutApi/services/orderCreator.js
@@ -96,9 +96,13 @@ exports.createOrder = async function createOrder(cart) {
       }
     });
 
+    const items = cart.getActiveItems();
+    const nextCart = { ...cart.exportData() };
+    nextCart.total_qty = items.length;
+
     const order = await insert('order')
       .given({
-        ...cart.exportData(),
+        ...nextCart,
         uuid: uuidv4().replace(/-/g, ''),
         order_number:
           10000 + parseInt(previous[0] ? previous[0].order_id : 0, 10) + 1,
@@ -109,7 +113,6 @@ exports.createOrder = async function createOrder(cart) {
       .execute(connection);
 
     // Save order items
-    const items = cart.getActiveItems();
     await Promise.all(
       items.map(async (item) => {
         const { is_active, ...itemData } = item.export(); // Destructure to remove 'is_active'
