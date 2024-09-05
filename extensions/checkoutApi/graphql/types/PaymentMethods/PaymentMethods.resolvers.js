2d96170a34c1d2b323bdeaee531d0bf316a3cdc4
Phung Ng Bao Long <phungngbaolong@gmail.com>
2024-09-05 10:02:48 +0700

    [feat] add payment method list for adyen

--- extensions/checkoutApi/graphql/types/PaymentMethods/PaymentMethods.resolvers.js
+++ extensions/checkoutApi/graphql/types/PaymentMethods/PaymentMethods.resolvers.js
@@ -1,13 +1,27 @@
 const { camelCase } = require('@evershop/evershop/src/lib/util/camelCase');
 const { getValue } = require('@evershop/evershop/src/lib/util/registry');
+const { getPaymentList } = require('adyen/services/adyen.service');
 
 module.exports = {
   Query: {
     paymentMethods: async () => {
       try {
         const fields = await getValue('paymentMethodList', []);
-
-        return fields;
+        const res = [];
+        for (const field of fields) {
+          if (field.code === 'adyen') {
+            const adyenList = await getPaymentList();
+            res.push({
+              ...field,
+              additionalData: {
+                supportedMethods: adyenList
+              }
+            });
+          } else {
+            res.push({ ...field });
+          }
+        }
+        return res;
       } catch (error) {
         return null;
       }
