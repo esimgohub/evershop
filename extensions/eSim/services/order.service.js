d15680e517c2eccda2d6266d4b9ad3f0b7b0dcf0
Phung Ng Bao Long <phungngbaolong@gmail.com>
2024-09-24 14:51:26 +0700

    [fix] module not found

--- extensions/eSim/services/order.service.js
+++ extensions/eSim/services/order.service.js
@@ -1,4 +1,4 @@
-const { select } = require('@evershop/postgres-query-builder');
+const { select, execute } = require('@evershop/postgres-query-builder');
 
 module.exports = {
   getOrderByOrderNum: async (uuid, pool) => {
@@ -30,6 +30,35 @@ module.exports = {
       return null;
     }
     return items;
+  },
+  getEsimDataType: async (productId, pool) => {
+    if (!productId) {
+      return null;
+    }
+    const rawQuery = `
+    select
+      *
+    from
+      product_attribute_value_index pavi
+    where
+      product_id = (
+      select
+      p.parent_product_id
+      from
+      product p
+      where
+      p.product_id = '${productId}'
+    )
+      and attribute_id in (
+      select
+      ao.attribute_id
+      from
+      attribute_option ao
+      where
+      ao.attribute_code = 'data-type'
+    )
+    `;
+    const { rows } = await execute(pool, rawQuery);
+    return rows?.length ? rows[0]?.option_text : null;
   }
-
 };
