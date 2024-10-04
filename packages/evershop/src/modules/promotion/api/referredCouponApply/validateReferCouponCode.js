a41a8f4c637a4984928dfe32285a4f98d2c0262c
Phung Ng Bao Long <phungngbaolong@gmail.com>
2024-10-04 16:08:21 +0700

    [fix]

--- packages/evershop/src/modules/promotion/api/referredCouponApply/validateReferCouponCode.js
+++ packages/evershop/src/modules/promotion/api/referredCouponApply/validateReferCouponCode.js
@@ -1,5 +1,4 @@
 const {
-  INVALID_PAYLOAD,
   INTERNAL_SERVER_ERROR,
   OK
 } = require('@evershop/evershop/src/lib/util/httpStatus');
@@ -9,34 +8,38 @@ const {
 const { select } = require('@evershop/postgres-query-builder');
 const { pool } = require('@evershop/evershop/src/lib/postgres/connection');
 
-module.exports = async (request, response) => {
+module.exports = async (request, response, _, next) => {
   try {
     if (!request.body.coupon || !/^\S*$/.test(request.body.coupon)) {
-      return response.status(INVALID_PAYLOAD).json({
+      response.status(OK).json({
         success: false,
         errorCode: CLIENT_CODE.COUPON_INVALID
       });
+      return;
     }
-    const query = await select()
+    const query = select()
       .from('coupon', 'c')
-      .where('c.coupon', '=', request.body.coupon)
-      .andWhere('c.status', '=', 1)
-      .andWhere('c.is_referral_code', '=', 1)
+      .where('c.coupon', '=', request.body.coupon);
+    query.andWhere('c.status', '=', 1);
+    query.andWhere('c.is_referral_code', '=', 1);
     const foundCoupon = await query.load(pool);
 
     if (!foundCoupon) {
-      return response.status(INVALID_PAYLOAD).json({
+      response.status(OK).json({
         success: false,
         errorCode: CLIENT_CODE.COUPON_INVALID
       });
+      return;
     }
 
-    return response.status(OK).json({
+    response.status(OK);
+    response.$body = {
       success: true,
       errorCode: CLIENT_CODE.OK
-    });
+    };
+    next();
   } catch (e) {
-    return response.status(INTERNAL_SERVER_ERROR).json({
+    response.status(INTERNAL_SERVER_ERROR).json({
       errorCode: CLIENT_CODE.INTERNAL_ERROR
     });
   }
