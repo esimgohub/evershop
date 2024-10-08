c0e6805daba576ee68d65c5bccfcfb0d994b9668
Phung Ng Bao Long <phungngbaolong@gmail.com>
2024-10-08 12:20:39 +0700

    [fix] minimum qty purchase

--- packages/evershop/src/modules/promotion/services/registerDefaultValidators.js
+++ packages/evershop/src/modules/promotion/services/registerDefaultValidators.js
@@ -93,8 +93,12 @@ module.exports.registerDefaultValidators =
         const minimumQty = !Number.isNaN(parseInt(conditions.order_qty, 10))
           ? parseInt(conditions.order_qty, 10)
           : null;
-        // todo: fix if check by active_item only
-        if (minimumQty && cart.getData('total_qty') < minimumQty) {
+        let totalQty = 0;
+        const items = cart.getActiveItems()
+        items.forEach((i) => {
+          totalQty += parseInt(i.getData('qty'), 10);
+        });
+        if (minimumQty && totalQty < minimumQty) {
           return false;
         }
         return true;
